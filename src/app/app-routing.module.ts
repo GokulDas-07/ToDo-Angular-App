84bd7ac75ee84ccb2b1d8b0286fe117148766c4f
Gokul Das M <Gokul@NTP-LAP-723.chn.nestgroup.net>
2022-11-25 12:36:41 +0530

    todo view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ToDoAddComponent } from './to-do-add/to-do-add.component';
+import { ToDoViewComponent } from './to-do-view/to-do-view.component';
 
 const routes: Routes = [
-  {path:"",component:ToDoAddComponent}
+  {path:"",component:ToDoAddComponent},
+  {path:"view",component:ToDoViewComponent}
 ];
 
 @NgModule({
